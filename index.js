7ccaf4f68bc5cf863fc05a92354245ddb201dab7
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2022-11-26 20:23:31 +0600

    basic mongodb connect and some data insert

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,23 @@ const app = express();
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dgcetkk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run () {
+    try{
+        const usedBikesMartCollection = client.db('usedBikesMart').collection('usedBikesData')
+    }
+    finally{
+
+    }
+}
+run().catch(console.log())
+
 
 app.get('/', async (req, res)=>{
     res.send('Used bikes mart server is running')
