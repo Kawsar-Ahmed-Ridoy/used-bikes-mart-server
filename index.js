8ae576f64c91f63ad6e424776865262f4042f2ce
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2022-11-26 18:42:38 +0600

    basic express server create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -7,3 +8,8 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+app.get('/', async (req, res)=>{
+    res.send('Used bikes mart server is running')
+})
+
+app.listen(port, () => console.log(`Used bikes mart running on ${port}`))
