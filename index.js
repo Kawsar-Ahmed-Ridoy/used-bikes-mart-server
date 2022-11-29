7fb89b15d2f0edcba767d0bd215a80f00560d885
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2022-11-29 20:02:32 +0600

    user reported items collection create

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -18,22 +18,20 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1
 });
 
-
-function verifyJWT(req, res, next){
+function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
-  if(!authHeader){
-      return res.status(401).send({message: 'unauthorized access'});
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
   }
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
-      if(err){
-          return res.status(403).send({message: 'Forbidden access'});
-      }
-      req.decoded = decoded;
-      next();
-  })
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
 }
-
 
 async function run() {
   try {
@@ -45,77 +43,88 @@ async function run() {
       .db("usedBikesMart")
       .collection("usedBikeData");
 
-      const usersCollection = client
-      .db("usedBikesMart")
-      .collection("users");
+    const usersCollection = client.db("usedBikesMart").collection("users");
 
-      const ordersBookCollection = client
+    const ordersBookCollection = client
       .db("usedBikesMart")
       .collection("ordersBook");
 
-      app.post('/jwt', (req, res) =>{
-        const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN)
-        res.send({token})
-    })
+    const reportedItemCollection = client
+      .db("usedBikesMart")
+      .collection("reportedItem");
 
-    app.put('/user/:email', async (req,res)=>{
-      const email = req.params.email
-      const user = req.body
-      const filter = {email: email}
-      const options = {upsert: true}
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN);
+      res.send({ token });
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
       const updateDoc = {
-        $set: user,
-      }
-      const result = await usersCollection.updateOne(filter, updateDoc, options)
+        $set: user
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       console.log(result);
 
       const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
-        expiresIn: '7d',
-      })
-      res.send({result, token})
-    })
+        expiresIn: "7d"
+      });
+      res.send({ result, token });
+    });
 
     app.get("/itemsKey", async (req, res) => {
-      const query = { };
+      const query = {};
       const bikesKey = await usedBikesItemCollection.find(query).toArray();
       res.send(bikesKey);
     });
 
     app.get("/usedBikeData/:id", async (req, res) => {
-      const category_id = parseInt(req.params.id)
-      const query = {category_id};
+      const category_id = parseInt(req.params.id);
+      const query = { category_id };
       console.log(query);
       const usedBikes = await usedBikesMartCollection.find(query).toArray();
       res.send(usedBikes);
     });
 
-    app.get('/ordersBook', verifyJWT, async(req, res)=>{
+    app.get("/ordersBook", verifyJWT, async (req, res) => {
       const decoded = req.decoded;
-      if(decoded.email !== req.query.email){
-          res.status(403).send({message: 'unauthorized access'})
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: "unauthorized access" });
       }
       let query = {};
-      if(req.query.email){
-          query = {
-              email: req.query.email
-          }
+      if (req.query.email) {
+        query = {
+          email: req.query.email
+        };
       }
       const cursor = ordersBookCollection.find(query);
       const ordersBook = await cursor.toArray();
       res.send(ordersBook);
-  });
+    });
 
-  app.post('/ordersBook', verifyJWT, async (req, res) =>{
-    const orders = req.body;
-    const result = await ordersBookCollection.insertOne(orders);
-    res.send(result);
-})
+    app.post("/ordersBook", verifyJWT, async (req, res) => {
+      const orders = req.body;
+      const result = await ordersBookCollection.insertOne(orders);
+      res.send(result);
+    });
 
+    app.post("/reportedItem", verifyJWT, async (req, res) => {
+      const report = req.body;
+      const result = await reportedItemCollection.insertOne(report);
+      res.send(result);
+    });
   } finally {
   }
 }
-run().catch(err =>console.log(err));
+run().catch((err) => console.log(err));
 
 app.get("/", async (req, res) => {
   res.send("Used bikes mart server is running");
